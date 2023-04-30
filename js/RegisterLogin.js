c032efc428d37acef684830e3ff24958bccb20b3
HelloCigar <jckarim@up.edu.ph>
2023-04-30 21:40:17 +0800

    testing login js

--- js/RegisterLogin.js
+++ js/RegisterLogin.js
@@ -4,39 +4,62 @@ function CreateUserAccount(username, password){
 	this.username = username;
 	this.password = password;
 };
+//check if username is already taken
+function userNameExits(arr , username){
+    for(let i = 0; i < arr.length; i++){
+        if((username) == arr[i].username){
+            alert("She's already taken");
+            return true;
+        }
+        return false;
+    }
 
 $(document).ready(function(){
-    $("#newAccount").submit(function(){
+    let testArray = [];
+    let createAccount = false;
+
+    $("#newAccount").submit(function(e){
+        e.preventDefault();
         //check if password and confirm password match
         if($("#password").val() != $("#repeat-password").val()){
             alert("Passwords do not match");
+            //clear the second password field
+            $("#repeat-password").val("");
+            createAccount = false;
+        }
+        //check if username is already taken
+        else if(testArray.length > 0 && userNameExits(testArray, $("#username").val())){
+            alert("Username already taken");
+            //clear the username and password fields
+            $("#username").val("");
+            $("#password").val("");
+            $("#repeat-password").val("");
+            createAccount = false;
         }
         else{
-            //alert("Submitted");
             //create a new user account object
             let newUser = new CreateUserAccount($("#username").val(), $("#password").val());
-            //store the user account object to a text file
+            //store the user account object to an array
             //temp solution before using a database
-            let blob = new Blob([JSON.stringify(newUser)], {type: "text/plain;charset=utf-8"});
-            saveAs(blob, "userAccounts/userAccounts.txt");
-        };
+            testArray.push(newUser);
+            console.log(testArray);
+        }
     });
 
     $("#login").submit(function(){
-        //check if username and password are found in the text file
-        //temp solution before using a database
-        let username = $("#loginUsername").val();
-        let password = $("#loginPassword").val();
-        let userAccountFound = false;
-        $.get("userAccounts.txt", function(data){
-            if(data.includes(username) && data.includes(password)){
-                userAccountFound = true;
+        //check if username and password match
+        let username = $("#username").val();
+        let password = $("#password").val();
+        let userFound = false;
+
+        for(let i = 0; i < testArray.length; i++){
+            if(username == testArray[i].username && password == testArray[i].password){
+                userFound = true;
+                break;
             }
-            else{
-                userAccountFound = false;
-            }
-        }, "text");
-        if(userAccountFound){
+        }
+
+        if(userFound){
             alert("Login successful");
         }
         else{
